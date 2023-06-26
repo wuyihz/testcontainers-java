c9b57cc33fd3341bb82debcdae8f664bc1d8b955
wuyi <wuyihz@126.com>
2023-06-26 10:56:33 +0800

    #init

--- core/src/main/java/org/testcontainers/DockerClientFactory.java
+++ core/src/main/java/org/testcontainers/DockerClientFactory.java
@@ -50,6 +50,7 @@ import java.util.function.Consumer;
 @Slf4j
 public class DockerClientFactory {
 
+
     public static final ThreadGroup TESTCONTAINERS_THREAD_GROUP = new ThreadGroup("testcontainers");
 
     public static final String TESTCONTAINERS_LABEL = DockerClientFactory.class.getPackage().getName();
